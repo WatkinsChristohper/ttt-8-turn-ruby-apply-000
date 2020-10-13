3051b13d69af62c0dd89c8228bc2849170b5414f
Christopher Watkins <chriswatkins7@yahoo.com>
2020-10-13 14:23:12 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,9 +1,4 @@
-def input_to_index(user_input)
-     input = user_input.to_i - 1 
-      input 
-    end
-
-   def display_board(board)
+def display_board(board)
      puts " #{board[0]} | #{board[1]} | #{board[2]} "
      puts "-----------"
      puts " #{board[3]} | #{board[4]} | #{board[5]} "
@@ -11,8 +6,13 @@ def input_to_index(user_input)
      puts " #{board[6]} | #{board[7]} | #{board[8]} "
      end
 
+def input_to_index(user_input)
+     input = user_input.to_i - 1 
+      input 
+    end
 
-  def position_taken?(array, index)
+
+def position_taken?(array, index)
      if array[index] == " " || array[index] == "" || array[index] == nil
         return false
      elsif array[index] == "X" || array[index] == "O"
@@ -20,12 +20,13 @@ def input_to_index(user_input)
      end
   end
 
-  def valid_move?(array, index)
+
+def valid_move?(array, index)
       if index.between?(0, 8) && !position_taken?(array, index)
        return true 
      end
   end
 
-  def move(array, index, character = "X")
+def move(array, index, character = "X")
      array[index] = character
   end
